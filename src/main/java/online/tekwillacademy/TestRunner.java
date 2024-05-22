4b97bbb6958426fdcfbcb6097cf94d3c0198e232
alexandrina <allexy.revenco@gmail.com>
2024-05-22 16:22:33 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -11,19 +11,19 @@ public class TestRunner {
 //        DriverManager manager = DriverManager.getInstance();
 //        WebDriver driver = manager.getDriver();
 
-        //
+        //Declararea driverului in mod optimizat
         WebDriver driver = DriverManager.getInstance().getDriver();
 
-        String currentTabName = driver.getCurrentUrl();
+        String currentTabName = driver.getWindowHandle();
 
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-safari-driver/4.20.0");
-        Thread.sleep(10000);
+        //Thread.sleep(10000);
         driver.close();
 
         driver.switchTo().window(currentTabName);
         driver.get("https://tekwill.md/tekwill-academy/");
-        Thread.sleep(10000);
+        //Thread.sleep(10000);
         driver.quit();
 
         System.out.println("The test is finished the driver is closed!");
